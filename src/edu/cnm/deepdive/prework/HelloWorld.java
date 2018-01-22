4ca6cd9f0ec5e1f19e75dfcef921650954773b00
ryanlazo <ryanlazo12@gmail.com>
2018-01-22 17:53:20 -0500

    salutation logic

---
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.prework;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    String target = salutees(args);
+    String message = salutation(target);
+    salute(message);
+  }
+
+  private static String salutees (String[] args) {
+    String result= "";
+    for (int i = 0; i < args.length; i++) {
+
+      result = result + args[i] + ",";
+    }
+    return result;
+  }
+  private static String salutation(String target) {
+    return "Hello, " + (!target.isEmpty()? target : "World");
+  }
+
+  private static void salute(String salutation) {
+    System.out.println(salutation);
+  }
+
+}
